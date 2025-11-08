44cfd23a99f2c2cf6ecc210fdf02e44df82ea834
appellatebaton0 <145923930+appellatebaton0@users.noreply.github.com>
2025-11-08 10:31:15 -0600

    v0.8 - Panes
    Started working on a pane system based on the pico game I made for siege this week, but way better. Will be a puzzle game, eventually.

--- dummies.lua
+++ dummies.lua
@@ -1,151 +1,160 @@
--- Basic Drawing Program
+-- Puzzle Game Test
 -- Baton0
 
-c = {
-    x=3,
-    o=12,
-    n=0
+settings = {
+    pane_fill = 6,
+    pane_outline = 0
 }
 
-printer = {
-    update = function (this)
-        if btnp(4, 1) then this:print() end
-    end,
+// Helper functions
+function rectline(x, y, size_x, size_y, fill, outline)
+    rectfill(x, y, x + size_x, y + size_y, fill)
+    rect(x, y, x + size_x, y + size_y, outline)
+end
 
-    print=function (this)
-        printh('{', 'drawings.txt')
+function inherit(target, from, is_array, afterword)
+    // Default the afterword to nothing.
+    afterword = afterword or function(target) end 
 
-
-        for i=1,grid.box_size do
-            row = "{"
-            for j=1,grid.box_size do
-                row = row..grid.box[i][j]..','
-
-                grid.box[i][j] = 0
+    if is_array then
+        for i,sub_target in pairs(target) do
+            if type(sub_target) == "table" do
+                setmetatable(sub_target, {__index=from})
+                afterword(sub_target)
             end
-            row = row.."},"
-            printh(row, 'drawings.txt')
         end
-
-        printh('},')
+    else
+        setmetatable(target, {__index=from})
+        afterword(target)
 
     end
+    
+end
+
+// States
+state = {
+    options = {},
+
+    _init = function(this, pane)
+
+    end,
+
+    _update = function(this, pane)
+
+    end,
+
+    _draw = function(this, pane)
+
+    end,
 }
 
-grid = {
 
-    offset = {x=4, y=4},
-    unit_size = 12,
-    box_size = 10,
+// Pane functions
+pane_value = 1
 
-    init=function(this)
-        this.box = {
+
+panes = {}
+pane = {
+    x=0,y=0,size_x=0,size_y=0,
+
+    state = "main",
+    change_state = function(this, to)
+        this.state = to
+    end,
+    init_states = function(this)
+        // Inherit the states.
+        inherit(this.states, state, true)
+
+        // Initialize the states.
+        for i, state in pairs(this.states) do
+            state:_init(this)
+        end
+    end,
+    states = {
+        main = {
+            _init = function(this, pane)
+
+            end,
+
+            _update = function(this, pane)
+
+            end,
+
+            _draw = function(this, pane)
+                rectline(pane.x, pane.y, 5, 5, 3, 0)
+            end,
         }
+    },
 
-        for i=1,this.box_size do
-            row = {}
-            for j=1,this.box_size do
-                row[j] = 0
-            end
-            this.box[i] = row
-        end
-
+    _init = function (this)
+        this:init_states()
     end,
 
-    draw=function(this)
-        for i=1,#this.box do
-            for j=1,#this.box do
-                left = this.offset.x + ((i-1) * this.unit_size)
-                right = this.offset.x + ((i) * this.unit_size)
-                top = this.offset.y + ((j-1) * this.unit_size)
-                bottom = this.offset.y + ((j) * this.unit_size)
+    _update = function(this)
+        // Update the current state.
+        this.states[this.state]:_update(this)
+    end,
 
-                rectfill(left,top,right,bottom, this.box[i][j]);
-                //rect(left,top,right,bottom, 0);
-            end
-        end
-    end
+    _draw = function(this)
+        rectline(this.x, this.y, this.size_x, this.size_y, settings.pane_fill, settings.pane_outline)
+
+        // Draw the current state.
+        this.states[this.state]:_draw(this)
+    end,
+
 }
 
-cursor = {
-    x=1, y=1, value=0,
+// -
 
-    control_cursor=function(this,clamp)
-        nx = this.x ny = this.y
+// Panes
 
-        if btn(0) then nx -= 1 this.timer = 20 end
-        if btn(1) then nx += 1 this.timer = 20 end
-        if btn(2) then ny -= 1 this.timer = 20 end
-        if btn(3) then ny += 1 this.timer = 20 end
-
-        if btnp(0,1) then nx -= 1 this.timer = 20 end
-        if btnp(1,1) then nx += 1 this.timer = 20 end
-        if btnp(2,1) then ny -= 1 this.timer = 20 end
-        if btnp(3,1) then ny += 1 this.timer = 20 end
-
-        if clamp then 
-            if nx > #grid.box then nx = #grid.box end
-            if nx < 1 then nx = 1 end
-            if ny > #grid.box then ny = #grid.box end
-            if ny < 1 then ny = 1 end
-        else
-            if nx > #grid.box then nx = 1 end
-            if nx < 1 then nx = #grid.box end
-            if ny > #grid.box then ny = 1 end
-            if ny < 1 then ny = #grid.box end
-        end
-        
-        this.x = nx 
-        this.y = ny
-    end,
-
-    write_cursor=function(this)
-        if btnp(4) then this.value += 1 this.timer = 20 end
-        if btnp(5) then this.value -= 1 this.timer = 20 end
-
-        if this.value > 15 then this.value = 0 end
-        if this.value < 0 then this.value = 15 end
-
-        if btn(5,1) then grid.box[this.x][this.y] = this.value this.timer = 20 end
-    end,
-
-    update=function(this, clamp)
-        this:control_cursor(clamp)
-        this:write_cursor()
-    end,
-
-    timer = 0,
-    draw=function(this)
-        this.timer+=1;
-
-        if this.timer > 15 then
-            left = grid.offset.x + ((this.x-1) * grid.unit_size)
-            right = grid.offset.x + ((this.x) * grid.unit_size)
-            top = grid.offset.y + ((this.y-1) * grid.unit_size)
-            bottom = grid.offset.y + ((this.y) * grid.unit_size)
-
-            rect(left - 1,top - 1,right,bottom, this.value);
-            
-            if this.timer > 30 then
-                this.timer = 0
-            end
-
-        end
-    end
+// The pane to store the puzzle solution.
+goal_pane = {
+    x = 4, y = 4, size_x = 31, size_y = 31
 }
+
+// The pane where the player solves the puzzle.
+puzzle_pane = {
+    x = 4, y = 40, size_x = 81, size_y = 81
+}
+
+// 
+
+
 
 function _init()
-    grid:init()
+    
+    // Inherit the pane as, well, panes.
+    inherit({
+        puzzle_pane,
+        goal_pane
+    }, pane, true, function(target)
+        // After inheritance, add it to the panes.
+        panes[pane_value] = target
+        pane_value += 1
+    end)
+
+    // Initialize the panes.
+    for i, pane in pairs(panes) do 
+        pane:_init()
+    end
 end
 
 function _update()
-    cursor:update(true)
-    printer:update()
+
+    // Update the panes.
+    for i, pane in pairs(panes) do 
+        pane:_update()
+    end
+
 end
 
 function _draw()
-    cls(5)
+    cls(7)
 
-    grid:draw()
-    cursor:draw()
+    // Draw the panes.
+    for i, pane in pairs(panes) do 
+        pane:_draw()
+    end
+
 end
