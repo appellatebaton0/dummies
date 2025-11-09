4d5152cab3c1da03519ad0655e652852c49c4321
appellatebaton0 <145923930+appellatebaton0@users.noreply.github.com>
2025-11-09 08:52:37 -0600

    v0.10 - Cleaner Physics
    Polished the physics a bit. I probably still won't use this, but it's cool and good practice.

--- dummies.lua
+++ dummies.lua
@@ -6,6 +6,19 @@ settings = {
     pane_outline = 0
 }
 
+function point_to_index(x, y, width)
+    return x + (y * width)
+end
+
+function index_to_point(i, width)
+    res = {}
+    j = i - 1
+    
+    res.x = (j % width)
+    res.y = flr(j / width)
+    return res
+end
+
 // -
 
 // Panes
@@ -34,10 +47,10 @@ goal_pane = {
             _draw = function(this, pane)
                 // Draw the contents of the grid to the screen.
                 for i=1,#pane.grid do
-                    j = i - 1
+                    point = index_to_point(i, pane.grid_width)
 
-                    x = (j % pane.grid_width)
-                    y = flr(j / pane.grid_width)
+                    x = point.x
+                    y = point.y
 
                     l = pane.x + (x * pane.pixel_size) + 1
                     t = pane.y + (y * pane.pixel_size) + 1
@@ -59,16 +72,16 @@ game_pane = {
     grid_width = 10,
     pixel_size = 8,
     grid = {
-        7,0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0,
+        0,7,0,7,0,0,0,0,0,0,
         0,7,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,4,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,
-        0,0,5,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,7,
+        0,7,0,0,0,0,0,0,0,0,
+        0,7,0,0,0,5,0,0,0,0,
+        0,7,0,0,0,0,0,0,0,0,
+        0,7,0,0,0,0,0,0,5,0,
+        0,7,0,0,0,0,0,0,0,0,
+        0,7,0,0,0,0,0,0,0,0,
+        0,7,0,0,0,0,0,0,0,0,
     },
 
     x = 4, y = 40, size_x = 81, size_y = 81,
@@ -96,13 +109,10 @@ game_pane = {
         }
     },
 
-    update_timer = 0,
-    _update = function(this)
-        // Update the current state.
-        this.states[this.state]:_update(this)
-
-        this.update_timer += 1
-        if this.update_timer >= 2 then 
+    gravity_timer = 0,
+    apply_gravity = function(this)
+        this.gravity_timer += 1
+        if this.gravity_timer >= 2 then 
             next_grid = {}
 
 
@@ -111,7 +121,7 @@ game_pane = {
             end
 
             for i=1,#this.grid do
-                if this.grid[i + this.grid_width] == 0 then 
+                if this.grid[i + this.grid_width] == 0 and this.grid[i] != 5 then 
                     //color = this.grid[i]
                     
                     next_grid[i + this.grid_width] = this.grid[i]
@@ -126,9 +136,51 @@ game_pane = {
                 this.grid[i] = next_grid[i]
             end
 
-            this.update_timer = 0
+            this.gravity_timer = 0
         end
 
+    end,
+
+    control_rotate = function(this)
+        if btnp(5) then
+            next_grid = {}
+
+
+            for i=1,#this.grid do
+                next_grid[i] = this.grid[i]
+            end
+
+            for i=1,#this.grid do
+                point = index_to_point(i, this.grid_width)
+
+                next_i = point_to_index(this.grid_width - point.y, point.x, this.grid_width) + 1
+
+                next_grid[next_i] = this.grid[i]
+
+            end
+
+            for i=1,#this.grid do
+                this.grid[i] = next_grid[i]
+            end
+        end
+            
+    end,
+
+    _init = function (this)
+        for i=1,this.grid_width ^ 2 do
+            this.grid[i] = 0 if i % 4 == 0 then this.grid[i] = 1 end
+
+        end
+
+        this:init_states()
+    end,
+    
+    _update = function(this)
+        // Update the current state.
+        this.states[this.state]:_update(this)
+
+        this:apply_gravity()
+        this:control_rotate()
 
         cursor:_update(this)
     end,
@@ -144,20 +196,37 @@ game_pane = {
 }
 
 cursor = {
-    x=4, y=9,
+    x=0, y=0,
+    cache = -1,
 
     clamp = function(this)
         this.x = max(0, min(this.x, game_pane.grid_width - 1))
         this.y = max(0, min(this.y, game_pane.grid_width - 1))
     end,
 
-    _update = function(this)
+    _update = function(this, pane)
         if btnp(0) then this.x -= 1 end
         if btnp(1) then this.x += 1 end
         if btnp(2) then this.y -= 1 end
         if btnp(3) then this.y += 1 end
 
         this:clamp()
+
+        if btnp(4) then
+            i = point_to_index(this.x, this.y, pane.grid_width) + 1
+
+            if this.cache == -1 then
+                if  pane.grid[i] != 0 then
+                
+
+                    this.cache = pane.grid[i]
+                    pane.grid[i] = 0
+                end
+            elseif pane.grid[i] == 0 then
+                pane.grid[i] = this.cache
+                this.cache = -1
+            end
+        end
     end,
 
     _draw = function (this, pane)
